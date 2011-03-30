555a520ccc4c1ab317512b7df1dbc558af05b8ee
Alexandra Andreea Vintila <andavintila@gmail.com>
2011-03-30 22:00:25 +0300

    processing database

--- DB/src/Main.java
+++ DB/src/Main.java
@@ -2,9 +2,6 @@ import java.io.*;
 import java.sql.*;
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static String querySelect(String tablename, String fieldname, String where, String like){
 		return "SELECT " + fieldname + " FROM " + tablename + " WHERE " + where + " LIKE "+ "'" + like + "'" + "AND type IS NULL "+" LIMIT 0,100;";
 	}
@@ -29,7 +26,7 @@ public class Main {
 		else if (type.contains("subst.")||type.contains("s."))
 			update = update("rontology.Def", "type", "s.", "id", id);
 		if (update != null){
-			//Statement stmt1 = con.createStatement();
+			Statement stmt1 = con.createStatement();
 			System.out.println("update");
 			stmt.executeUpdate(update);
 		}
@@ -41,28 +38,23 @@ public class Main {
 			update = update("rontology.Def", "type", "adj. adv.", "id", id);
 		else if (type.contains("vb."))
 			update = update("rontology.Def", "type", "adj. vb.", "id", id);
-		/*else if (type.contains("adj."))
-			update = update("rontology.Def", "type", "adv. adj.", "id", id);*/
+		else if (type.contains("adj."))
+			update = update("rontology.Def", "type", "adv. adj.", "id", id);
 		else if (type.contains("subst.")||type.contains("s."))
 			update = update("rontology.Def", "type", "adj. s.", "id", id);
 		if (update != null){
-			//Statement stmt1 = con.createStatement();
+			Statement stmt1 = con.createStatement();
 			System.out.println("update");
 			stmt.executeUpdate(update);
 		}
 	}
 	
-	public static String prelCar(String s, String c){
-		
-			return s;
-	}
-	
-	public static void prelucrareFis() throws IOException, SQLException, ClassNotFoundException{
+	public static void processFile() throws IOException, SQLException, ClassNotFoundException{
 		String username = "root";
 		String password = "ceva";
-	//<connection-url>jdbc:mysql://localhost:3306/cmobservatorio?useUnicode=true&amp;characterEncoding=U TF-8</connection-url>
 		String url = "jdbc:mysql://127.0.0.1/rontology?autoReconnect=true&useUnicode=true&characterEncoding=utf8";
 		Class.forName("com.mysql.jdbc.Driver");
+		
 		Connection con = DriverManager.getConnection(url, username, password);
 		BufferedReader br = new BufferedReader( new FileReader("bd.txt"));
 		int i = 0, id = 386857, nrW = 1;
@@ -71,13 +63,8 @@ public class Main {
 		ResultSet rs;
 		String linie=null;
 		int cont = 0;
-		while(cont!=1315070){
-			cont++;
-			linie = br.readLine();
-		}
-		System.out.println(linie);
+		
 		String line = br.readLine();
-		System.out.println(line);
 		
 		while (line != null){
 			String[] word = line.split(" ~~ ");
@@ -103,7 +90,6 @@ public class Main {
 					}
 					line = br.readLine(); 
 					while (line.isEmpty() != true){
-						//System.out.println(line);
 						String def = null;
 						String[] words = line.split("\\. {1,}+");
 						if (words.length >= 2){
@@ -125,9 +111,6 @@ public class Main {
 							else wordDef = words[1];
 							}
 						
-				//		System.out.println(wordDef);
-					//	else wordDef = words[1].split("{3} ")[1];
-						
 						Statement stmt1 = con.createStatement();
 						ResultSet rs1 = stmt1.executeQuery("SELECT * FROM rontology.Def WHERE lexicon LIKE '" + wordDef +"'AND type LIKE '%"+ type +"%';");
 						String wds = null;
@@ -137,7 +120,6 @@ public class Main {
 						if (rs1.next()) def = rs1.getString(4);
 						if (def != null && def.contains("'")){
 							String[] spl = def.split("'");
-						//	System.out.println(spl[0]);
 							def = spl[0];
 							for (i = 1; i < spl.length; i++){
 								if(def.endsWith("\\"))
@@ -145,27 +127,27 @@ public class Main {
 								else def = def.concat("''").concat(spl[i]);
 							}
 						}
+						
 						sql = "insert into rontology.Synset (id, type, nrWords, words, definition) values(" + id + ", '" + type + "', " + nrW + ", '" + wds + "', '"+ def +"');";
 						Statement stmt3 = con.createStatement();
 						ResultSet rs3 = stmt3.executeQuery("SELECT * FROM rontology.Synset WHERE words LIKE '" + wds + "';");
+						
 						if (rs3.next() == false){
 							System.out.println(sql);
 							stmt.executeUpdate(sql);
 						}
+						
 						rs1.close();
 						stmt1.close();
 						rs3.close();
 						stmt3.close();
 						}
-						//= stmt.executeUpdate(sql);
 						line = br.readLine();
 						id++;
 						nrW = 1;
 						
 					}
-					//br.readLine();
 					line = br.readLine();
-					//fw = line;
 					if (line.isEmpty()!= true) line = br.readLine();
 				}
 				 
@@ -180,7 +162,6 @@ public class Main {
 					def = rs2.getString(4);
 					if (def != null && def.contains("'")){
 						String[] spl = def.split("'");
-					//	System.out.println(spl[0]);
 						def = spl[0];
 						for (i = 1; i < spl.length; i++){
 							if(def.endsWith("\\"))
@@ -202,9 +183,6 @@ public class Main {
 				stmt2.close();
 				stmt4.close();
 				}
-				//System.out.println(word[1]);
-				//System.out.println(sql);
-				//System.out.println("(" + id + ", '" + type + "', " + nrW + ", '" + word[1]+"', '"+ def +"');");
 				id++;
 				
 			}
@@ -218,95 +196,6 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-	//	System.out.println("ceva");
-		String username = "root";
-		String password = "ceva";
-	//<connection-url>jdbc:mysql://localhost:3306/cmobservatorio?useUnicode=true&amp;characterEncoding=U TF-8</connection-url>
-		String url = "jdbc:mysql://127.0.0.1/rontology?autoReconnect=true&useUnicode=true&characterEncoding=utf8";
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(url, username, password);
-		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("");
-		
-		//tip adj si adv
-	//	String query = querySelect("SELECT *  FROM rontology.Def WHERE htmlRep LIKE '%adj.%' AND htmlRep LIKE '%adv.%';");
-	/*	ResultSet rs = stmt.executeQuery("SELECT * FROM rontology.Def WHERE id <= 534282 AND id >= 500000;");
-		while (rs.next()){
-			//System.out.print(rs.getString(3) + ", ");
-			Statement stmt1 = con.createStatement();
-			ResultSet rs1 = stmt1.executeQuery("SELECT * FROM dex.LexemDefinitionMap WHERE definitionId = '" + rs.getString(1) + "' ;" );
-			if (rs1.next()){
-				//"SELECT * FROM dex.InflectedForm WHERE lexemId = '" + rs.getString(1) + "' ;" );
-				//System.out.println(rs.getString(1) + "    " + rs1.getString(2));
-				
-				Statement stmt3 = con.createStatement();
-				ResultSet rs3 = stmt3.executeQuery("SELECT * FROM dex.InflectedForm WHERE lexemId = '" + rs1.getString(2) + "' ;" );
-				if (rs3.next()){
-				int infl = Integer.parseInt(rs3.getString(6));
-			//	System.out.println("infl" + infl);
-				String update = null;
-				if (infl <= 8) 
-					update = update("rontology.Def", "type", "s.m.", "id", rs.getString(1));
-				else if (infl <= 16)
-					update = update("rontology.Def", "type", "s.f.", "id", rs.getString(1));
-				else if (infl <= 24)
-					update = update("rontology.Def", "type", "s.n.", "id", rs.getString(1));
-				else if (infl <= 40)
-					update = update("rontology.Def", "type", "adj.", "id", rs.getString(1));
-				else if (infl < 84 && infl >= 49)
-					update = update("rontology.Def", "type", "vb.", "id", rs.getString(1));
-				else if (infl == 84)
-					update = update("rontology.Def", "type", "inv.", "id", rs.getString(1));
-				Statement stmt2 = con.createStatement();
-				if (update != null){
-				//	System.out.println(update);
-					stmt2.executeUpdate(update);
-				}
-			}}
-		}
-		
-	/*	//tip adv 
-		String query = querySelect("rontology.Def", "*", "htmlRep", "%adv.%" );
-		ResultSet rs = stmt.executeQuery(query);
-		while (rs.next()){
-			System.out.print(rs.getString(3) + ", ");
-			String update = update("rontology.Def", "type", "adv", "id", rs.getString(1)); 
-			Statement stmt2 = con.createStatement();
-			System.out.println(stmt2.executeUpdate(update));
-		}*/
-	/*	//SELECT * FROM `Def` WHERE type like 'adj. vb.' and internalRep not like '%adj.%'
-		ResultSet rs = stmt.executeQuery("SELECT * FROM rontology.Def WHERE type like 'adj. vb.' and internalRep not like '%adj.%'");
-	//	System.out.println(rs.getString(1));
-		while (rs.next()){
-			System.out.println(rs.getString(1));
-			Statement stmt1 = con.createStatement();
-	//		String update = update("rontology.Def", "type", "vb.", "id", rs.getString(1));
-		//	stmt1.executeUpdate(update);
-		//	System.out.println(rs.getString(4));
-		//	String[] type = rs.getString(4).split("@");
-		//	if (type.length >= 3){
-			//	System.out.println(type[2]);
-				type = type[2].split(" ");
-				if (type.length >= 5){
-			 		System.out.println(type[4]);
-			 	//	changeType2(type[3], rs.getString(1), stmt1);
-					}
-			}
-		//	for (int i = 0; i < type.length; i++)
-			//	changeType(type[i], rs.getString(1), stmt1);
-			
-			
-		//	update = update("rontology.Def", "type", type[0], "id", rs.getString(1));*/
-	//	Statement stmt = con.createStatement();
-	/*	ResultSet rs = stmt.executeQuery("select * from Def where lexicon like 'deține';");
-		while (rs.next()){
-	//		System.out.println(rs.getString(4));
-		}*/
-		//prelucrareFis();
-//		stmt.close();
-	//	con.close();
-		}
-	//metacaracter
-	//@dexonline
-}
+		processFile();
+	}
 
